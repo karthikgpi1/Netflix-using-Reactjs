e3fbbff4e1bdbdbe10d6f2e638646615ef312d39
karthikgpi1 <karthikgopi024@gmail.com>
2022-09-27 16:37:51 +0530

    main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,7 +12,15 @@ const Main = () => {
     });
   }, []);
 
-  console.log(movie);
+  // console.log(movie);
+
+  const truncateString = (str, num) => {
+    if (str?.length > num) {
+      return str.slice(0, num) + ".....";
+    } else {
+      return str;
+    }
+  };
 
   return (
     <div className="w-full h-[550px] text-white">
@@ -25,7 +33,7 @@ const Main = () => {
         />
         <div className="absolute w-full top-[20%] p-4 md:p-8">
           <h1 className="text-3xl md:text-5xl font-bold">{movie?.title}</h1>
-          <div>
+          <div className="my-4">
             <button className="border bg-gray-300 text-black border-gray-300 py-2 px-5">
               Play
             </button>
@@ -33,6 +41,12 @@ const Main = () => {
               Watch Later
             </button>
           </div>
+          <p className="text-gray-400 text-sm">
+            Released: {movie?.release_date}
+          </p>
+          <p className="w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200">
+            {truncateString(movie?.overview, 150)}
+          </p>
         </div>
       </div>
     </div>
