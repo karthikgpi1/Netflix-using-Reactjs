5ab9d761cbbaf7241e93ca8fa190d41986757683
karthikgpi1 <karthikgopi024@gmail.com>
2022-09-27 16:17:44 +0530

    fetching movie api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Navbar from "./components/Navbar";
 import "./index.css";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
