accc090d4ec3fdfc2232dc104cbf2cc357959c90
karthikgpi1 <karthikgopi024@gmail.com>
2022-09-28 12:05:06 +0530

    SignUp

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import "./index.css";
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Account from "./pages/Account";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/account" element={<Account />} />
         </Routes>
       </AuthContextProvider>
     </>
